ed0f80261d18c46cf50e70f16c95a304996cf109
anas12khan <124598699+anas12khan@users.noreply.github.com>
2023-02-06 21:28:25 +0530

    commit

---
+++ newsapp/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./navbar/navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ newsapp/src/navbar/navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-10">
+      <div className="text-black text-3xl">hello</div>
+    </div>
+  );
+};
+
+export default Navbar;
