0918b57695ad6dd847ea01816fe1dc4a72d9d203
anas12khan <124598699+anas12khan@users.noreply.github.com>
2023-02-07 22:32:32 +0530

    commit

--- newsapp/src/component/navbar/navbar.js
+++ newsapp/src/component/navbar/navbar.js
@@ -3,9 +3,13 @@ import { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { GiHamburgerMenu } from "react-icons/gi";
 import Sidebar from "../sidebar/sidebar";
+import data from "../data.json";
 const Navbar = () => {
   const [change, setChange] = useState("Vendor");
   const myArray = { id: ["apple", "banana", "orange"] };
+  const [name, setName] = useState();
+
+  console.log(name);
 
   const handleChange = () => {
     if (change === "Vendor") {
@@ -14,31 +18,37 @@ const Navbar = () => {
       setChange("Vendor");
     }
   };
+  const numbers = [1, 2, 3, 4, 5, 6];
+
+  const [one, two, ...rest] = numbers;
+
   return (
     <div className="flex">
-      {change === "Vendor" ? (
-        <div className="flex justify-between">
-          <div className="flex text-3xl">
-            <GiHamburgerMenu onClick={handleChange} />
-            <AiOutlineSearch />
-          </div>
-          <div>
-            <h1 className="text-3xl">{change}</h1>
-          </div>
-          {myArray.id.map(
-            (item, key) => (
-              console.log(key),
-              (
-                <div className="p-10 flex justify-between">
-                  <button className="text-2xl">{item}</button>
-                </div>
-              )
-            )
-          )}
+      {/* <div className="flex justify-between">
+        <div className="flex text-3xl">
+          <GiHamburgerMenu
+            onClick={(e) => {
+              console.log(e);
+            }}
+          />
+          <AiOutlineSearch />
         </div>
-      ) : (
-        <Sidebar text={change} value="abc" />
-      )}
+        <div>
+          <h1 className="text-3xl">{change}</h1>
+        </div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {myArray.id.map((item, key) => (
+          <div className="p-10 flex justify-between">
+            <button className="text-2xl">{item}</button>
+          </div>
+        ))}
+      </div> */}
+
+      <Sidebar text={data} valueFunction={handleChange} change={setChange} />
     </div>
   );
 };
--- newsapp/src/component/sidebar/sidebar.js
+++ newsapp/src/component/sidebar/sidebar.js
@@ -1,11 +1,55 @@
 import React from "react";
 
 const Sidebar = (props) => {
-  console.log(props);
+  const arraySecond = {
+    secondArticles: [
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+      { name: "xyz", type: "lorem", disc: "lorem" },
+    ],
+  };
+
+  console.log(props?.text);
   return (
-    <div>
-      <div>{props.text}</div>
-      <div>{props.value}</div>
+    <div className="w-full">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props?.text?.articles?.map((item, key) => {
+            return (
+              <tr>
+                <th scope="row">{key}</th>
+                <td>{item.author}</td>
+                <td>{item.description}</td>
+                <td>{item.content}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      <div>
+        <button>First</button>
+        <button>second</button>
+        <button>Third</button>
+      </div>
     </div>
   );
 };
